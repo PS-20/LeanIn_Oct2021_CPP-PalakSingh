fee26ef184aef6db3b532e76e5c36978f099fa4d
PS-20 <82272824+PS-20@users.noreply.github.com>
2021-10-13 01:16:28 +0530

    Q2.

---
+++ Milestone-1/No. entered by user.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter a number : ";
+	cin>>n;
+	cout<<"Your number : ";
+	cout<<n;
+	return 0;
+	
+}
+
+/* OUTPUT
+Enter a number : 2
+Your number : 2 /*
