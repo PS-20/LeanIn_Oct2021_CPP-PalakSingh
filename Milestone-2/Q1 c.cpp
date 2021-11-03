e89b0021d67370179f9c24c074711fb6afa90284
PS-20 <82272824+PS-20@users.noreply.github.com>
2021-11-03 13:17:20 +0530

    Q1 c

---
+++ Milestone-2/Q1 c.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+/* OUTPUT 
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5  */
