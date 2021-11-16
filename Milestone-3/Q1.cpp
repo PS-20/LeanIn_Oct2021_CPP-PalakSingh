830a2a47a331576a9e05c5d6e031b843b819d0e5
PS-20 <82272824+PS-20@users.noreply.github.com>
2021-11-16 11:28:36 +0530

    Q1

---
+++ Milestone-3/Q1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	char a[n]={'a','e','i','o','u'};
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
+
+/* OUTPUT
+
+a e i o u
+
+*/
